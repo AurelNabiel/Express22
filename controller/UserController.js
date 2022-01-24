0e9a95b3fecba9c681df56de919e7b9f4bd8e321
unknown <you@example.com>
2022-01-24 14:48:27 +0700

    24-01-2022

--- controller/UserController.js
+++ controller/UserController.js
@@ -30,7 +30,6 @@ const detail = async (req, res) => {
       return res.json({
         status: "Gagal",
         messege: "Data User Tidak Ditemukan",
-     
       });
     }
     return res.json({
@@ -47,4 +46,94 @@ const detail = async (req, res) => {
   }
 };
 
-module.exports = { index, detail };
+const detailByEmail = async (req, res) => {
+  try {
+    const { email } = req.params;
+    const users = await UserModel.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (users === null) {
+      return res.status(200).json({
+        status: "Gagal",
+        msg: "Data User tidak ditemukan",
+      });
+    }
+    return res.json({
+      status: "Berhasil",
+      msg: "Data User ditemukan",
+      data: users,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(403).json({
+      status: "Gagal",
+      msg: "ada Sebuah kesalahan",
+    });
+  }
+};
+
+const destroy = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const users = await UserModel.destroy({
+      where: {
+        id: id,
+      },
+    });
+    if (users === 0) {
+      return res.status(200).json({
+        status: "Gagal",
+        msg: "Data User tidak ditemukan",
+      });
+    }
+    return res.json({
+      status: "Berhasil",
+      msg: "Data User ditemukan",
+      data: users,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(403).json({
+      status: "Gagal",
+      msg: "ada Sebuah kesalahan",
+    });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    const users = await UserModel.findByPk(id);
+    if (users === null) {
+      return res.json({
+        status: "Gagal",
+        msg: "Data User tidak ditemukan",
+      });
+    }
+    await UserModel.update(
+      {
+        name: name,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    return res.json({
+      status : "berhasil",
+      msg : "Data User berhasil diperbarui"
+    })
+  } catch (err) {
+    console.log(error);
+    return res.status(403).json({
+      status: "Gagal",
+      msg: "ada Sebuah kesalahan",
+    });
+  }
+};
+
+module.exports = { index, detail, detailByEmail, destroy, update };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { register } = require("../controller/AuthController");
-const { index, detail } = require("../controller/UserController");
-
+const { index, detail, detailByEmail, destroy, update } = require("../controller/UserController");
 const validationMiddleware = require("../middleware/ValidationMiddleware");
 const { registerValidator } = require("../validator/AuthValidator");
 const router = express.Router();
@@ -15,18 +14,17 @@ router.get("/", (req, res) => {
 
 // GET USER ALL //
 router.get('/users',index)
-
 // GET USER DETAIL // 
 router.get('/users/:id',detail)
+//GET USER EMAIL//
+router.get('/users/email/:email',detailByEmail)
+//DELETE//
+router.delete('/users/:id',destroy)
+// UPDATE //
+router.put('/users/update/:id',update)
 
 // REGISTER //
 router.post("/register", registerValidator, validationMiddleware, register);
-
-
-
-
-
-
 
 
 module.exports = router;
